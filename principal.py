bae8388f9ab3ee6db88b1dc5085df7abdf687435
Juls7 <73595751+Juls7@users.noreply.github.com>
2020-11-28 17:37:00 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -2,7 +2,7 @@ from procedimientos.preguntas import preguntar, llenar_datos
 from procedimientos.resultados import result
 from validaciones.validar import val_opcion
 import pandas as pd
-#se necesita instalar tabulate, si no lo tiene pegue el siguiente link en su terminal: conda install -c conda-forge tabulate
+#se necesita instalar tabulate, pegue el siguiente link es u terminal: conda install -c conda-forge tabulate
 
 #   El programa empieza pidiendole al usuario datos personales tales como:
 #   nombre. cedula. edad. genero (1 masculino, 0 femenino) 
@@ -14,7 +14,7 @@ def main():
     """
     ruta = "./datos/datos.json"
     ruta_preguntas = "./datos/respuestas.json"
-    print("\n Se necesita instalar tabulate, si no lo tiene pegue el siguiente link en su terminal: conda install -c conda-forge tabulate")    
+    print("\n Se necesita instalar tabulate: conda install -c conda-forge tabulate")
     print('\n\n\t\tTest Vocacional\n\n')
     opcion = val_opcion('Presione enter para empezar con la encuesta 0 de lo contrario --->')
     while opcion != '0':
@@ -24,15 +24,11 @@ def main():
             result(cedula, ruta)
             opcion = val_opcion('Presione enter para empezar con la encuesta, 1 para ver los datos guardados y 0 para salir --->')
         if opcion =='1':
-            datos=pd.read_csv("datos voc.csv", names=['cedula','nombre','edad','genero'])
-            categoria=pd.read_csv("voc.csv", names=['categoria'])
-            eleccion=pd.read_csv("seleccion prof.csv", names=['elec_1','elec_2','elec_3'])
-            compatible=pd.read_csv("compa.csv", names=['comp_1','comp_2','comp_3','comp_4','comp_5','comp_6','comp_7'])
-            print(pd.concat([datos, categoria, eleccion, compatible], axis=1))
+            datos=pd.read_csv("datos voc.csv", names=['cedula','nombre','edad','genero','categoria','elec_1','elec_2','elec_3','comp_1','comp_2','comp_3','comp_4','comp_5','comp_6','comp_7'])
+            print(datos)
             opcion = val_opcion('Presione enter para empezar con la encuesta, 1 para ver los datos guardados y 0 para salir --->')
     print('Hasta luego')    
 
 main()
-
 
 
